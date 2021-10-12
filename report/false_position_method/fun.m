d1ad20594d4646c102ea2948f395e972b8110dd5
ydoo123 <ydoo123@gmail.com>
2021-10-12 22:35:36 +0900

    message

---
+++ report/false_position_method/fun.m
@@ -0,0 +1,3 @@
+function y=fun(x)
+y=x^3-x^2-x-1;
+end
---
+++ report/false_position_method_modified/main.m
@@ -0,0 +1,45 @@
+clear
+clc
+
+x_l = 1;
+x_u = 2;
+tol_x = 0.001;
+tol_y = 0.00001;
+
+y_l = fun(x_l);
+y_u = fun(x_u);
+
+if (y_l*y_u < 0)
+    fprintf('again\n');
+    return
+end
+
+iter = 0;
+x_r = 0;
+
+while(1)
+    iter = iter + 1;
+    x_r = (x_l*y_u - x_u*y_l)/(y_u - y_l);
+    y_r = fun(x_r);
+    
+    if (y_l*y_r < 0)
+        if(abs(x_r - x_u) < tol_x || abs(y_r) < tol_y)
+            fprintf(1,'Approximate solution x_r= %.7f \n', x_r);
+            break
+       
+        else
+            x_u = x_r;
+            y_u = y_r;
+        end
+        
+    else
+        if(abs(x_l - x_r) < tol_x || abs(y_r) < tol_y)
+            fprintf(1,'Approximate solution x_r= %.7f \n', x_r);
+            break
+            
+        else
+            x_l = x_r;
+            y_l = y_r;
+        end
+    end
+end
